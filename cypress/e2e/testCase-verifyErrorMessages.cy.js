48b588658dba7c769054d326e88f0210a5cc6d00
nagularapu1982 <nagularapu1982@gmail.com>
2022-12-15 17:13:16 +0000

    Update testCase-verifyErrorMessages.cy.js

--- cypress/e2e/testCase-verifyErrorMessages.cy.js
+++ cypress/e2e/testCase-verifyErrorMessages.cy.js
@@ -7,11 +7,9 @@ describe('Verify error messages',()=>{
         cy.visit('https://shop.demoqa.com/my-account/')
         const homePage = new HomePage();
         homePage.getLogin().should('have.text','Log in').click()
-// <<<<<<< HEAD
-//         cy.log("login button is test correct")
-// =======
+
         cy.log("login button in feature")
-//>>>>>>> feature
+
     
         homePage.getusernameErrormessage().contains('Error: Username is required.').scrollIntoView()
 })
